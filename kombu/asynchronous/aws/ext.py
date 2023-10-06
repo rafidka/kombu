8eecce6f424caed2e6aca521ac4e721bef20f117
Rafid Al-Humaimidi <rafidka@gmail.com>
2023-10-06 05:27:58 +0000

    [WIP] botocore-inspired implementation of get_response

--- kombu/asynchronous/aws/ext.py
+++ kombu/asynchronous/aws/ext.py
@@ -6,7 +6,7 @@ try:
     import boto3
     from botocore import exceptions
     from botocore.awsrequest import AWSRequest
-    from botocore.response import get_response
+    from botocore import parsers
 except ImportError:
     boto3 = None
 
@@ -18,9 +18,20 @@ except ImportError:
     exceptions = _void()
     exceptions.BotoCoreError = BotoCoreError
     AWSRequest = _void()
-    get_response = _void()
 
 
 __all__ = (
     'exceptions', 'AWSRequest', 'get_response'
 )
+
+def get_response(operation_model, http_response):
+    response_dict = {
+        'headers': http_response.headers,
+        'status_code': http_response.status_code,
+    }
+    response_dict['body'] = http_response.content
+
+    parser = parsers.create_parser('query')
+    return http_response, parser.parse(
+        response_dict, operation_model.output_shape
+    )
